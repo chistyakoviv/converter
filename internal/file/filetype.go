c36486f7868a86726905de43e6e360bea5801dec
Ilya Chistyakov <ichistyakovv@gmail.com>
2024-11-21 18:01:59 +0500

    refactor: move common code to a private function from the file type checking functions

--- internal/file/filetype.go
+++ internal/file/filetype.go
@@ -9,36 +9,37 @@ import (
 
 const headSize = 261
 
-func IsImage(src string) (bool, error) {
+func readHead(src string) ([]byte, error) {
 	wd, err := os.Getwd()
 	if err != nil {
-		return false, err
+		return nil, err
 	}
 	file, err := os.Open(fmt.Sprintf("%s%s", wd, src))
 	if err != nil {
-		return false, err
+		return nil, err
 	}
 	defer file.Close()
 
 	head := make([]byte, headSize)
 	file.Read(head)
+
+	return head, nil
+}
+
+func IsImage(src string) (bool, error) {
+	head, err := readHead(src)
+	if err != nil {
+		return false, err
+	}
 
 	return filetype.IsImage(head), nil
 }
 
 func IsVideo(src string) (bool, error) {
-	wd, err := os.Getwd()
+	head, err := readHead(src)
 	if err != nil {
 		return false, err
 	}
-	file, err := os.Open(fmt.Sprintf("%s%s", wd, src))
-	if err != nil {
-		return false, err
-	}
-	defer file.Close()
-
-	head := make([]byte, headSize)
-	file.Read(head)
 
 	return filetype.IsVideo(head), nil
 }
