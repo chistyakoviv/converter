fdc3244a91763d4019e7fb311c67dc12312ef758
Ilya Chistyakov <ichistyakovv@gmail.com>
2024-11-21 17:27:27 +0500

    chore: cleanup

--- internal/converter/converter.go
+++ internal/converter/converter.go
@@ -1,6 +1,5 @@
 package converter
 
-// TODO: rewrite to use common interface for both image and video
 type ImageConverter interface {
 	Shutdowner
 	Convert(from string, to string, conf ConversionConfig) error
