b343b5529bed29810c7da4a6f89dcfb93edb39f2
Ilya Chistyakov <ichistyakovv@gmail.com>
2024-12-20 21:50:13 +0500

    test: add unit tests for conversion queue service

--- internal/service/conversionq/tests/conversionq_test.go
+++ internal/service/conversionq/tests/conversionq_test.go
@@ -6,21 +6,26 @@ import (
 	"testing"
 
 	"github.com/chistyakoviv/converter/internal/config"
+	"github.com/chistyakoviv/converter/internal/db"
 	dbMocks "github.com/chistyakoviv/converter/internal/db/mocks"
 	"github.com/chistyakoviv/converter/internal/model"
 	repositoryMocks "github.com/chistyakoviv/converter/internal/repository/mocks"
+	"github.com/chistyakoviv/converter/internal/service"
 	"github.com/chistyakoviv/converter/internal/service/conversionq"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/mock"
 )
 
+var (
+	ctx          = context.Background()
+	configPath   = "config/local.yaml"
+	defaultsPath = "config/defaults.yaml"
+)
+
 func TestAddToConversionQueue(t *testing.T) {
 	var (
-		ctx                     = context.Background()
 		errorId           int64 = -1
 		successId         int64 = 0
-		configPath              = "config/local.yaml"
-		defaultsPath            = "config/defaults.yaml"
 		defaultCfg              = config.MustLoad(configPath, defaultsPath)
 		jpgConversionInfo       = func() *model.ConversionInfo {
 			// Generate models inside a function, because the conversion queue modifies the model passed to the Add method
@@ -202,7 +207,7 @@ func TestAddToConversionQueue(t *testing.T) {
 
 	for _, tc := range cases {
 		t.Run(tc.name, func(t *testing.T) {
-			tc.mockConversionRepository(&tc)
+			t.Parallel()
 
 			mockConversionRepository := tc.mockConversionRepository(&tc)
 			mockTxManager := tc.mockTxManager(&tc)
@@ -224,6 +229,296 @@ func TestAddToConversionQueue(t *testing.T) {
 
 			if tc.convertTo != nil {
 				assert.Equal(t, tc.convertTo, tc.conversionInfo.ConvertTo)
+			}
+
+			mockConversionRepository.AssertExpectations(t)
+			mockTxManager.AssertExpectations(t)
+		})
+	}
+}
+
+func TestPopFromConversionQueue(t *testing.T) {
+	var (
+		conversion = &model.Conversion{
+			Fullpath: "/files/images/gen.jpg",
+			Path:     "/files/images",
+			Filestem: "gen",
+			Ext:      "jpg",
+		}
+	)
+
+	type testcase struct {
+		name                     string
+		err                      error
+		conversion               *model.Conversion
+		mockConversionRepository func(tc *testcase) *repositoryMocks.MockConversionQueueRepository
+		mockTxManager            func(tc *testcase) *dbMocks.MockTxManager
+	}
+
+	cases := []testcase{
+		{
+			name:       "Empty conversion queue",
+			err:        db.ErrNotFound,
+			conversion: conversion,
+			mockConversionRepository: func(tc *testcase) *repositoryMocks.MockConversionQueueRepository {
+				mockConversionRepository := repositoryMocks.NewMockConversionQueueRepository(t)
+				mockConversionRepository.On("FindOldestQueued", mock.AnythingOfType("context.backgroundCtx")).Return(nil, db.ErrNotFound)
+				return mockConversionRepository
+			},
+			mockTxManager: func(tc *testcase) *dbMocks.MockTxManager {
+				mockTxManager := dbMocks.NewMockTxManager(t)
+				return mockTxManager
+			},
+		},
+		{
+			name:       "Successful pop from conversion queue",
+			conversion: conversion,
+			mockConversionRepository: func(tc *testcase) *repositoryMocks.MockConversionQueueRepository {
+				mockConversionRepository := repositoryMocks.NewMockConversionQueueRepository(t)
+				mockConversionRepository.On("FindOldestQueued", mock.AnythingOfType("context.backgroundCtx")).Return(conversion, nil)
+				return mockConversionRepository
+			},
+			mockTxManager: func(tc *testcase) *dbMocks.MockTxManager {
+				mockTxManager := dbMocks.NewMockTxManager(t)
+				return mockTxManager
+			},
+		},
+	}
+
+	for _, tc := range cases {
+		t.Run(tc.name, func(t *testing.T) {
+			t.Parallel()
+
+			mockConversionRepository := tc.mockConversionRepository(&tc)
+			mockTxManager := tc.mockTxManager(&tc)
+
+			serv := conversionq.NewService(
+				config.MustLoad(configPath, defaultsPath),
+				mockTxManager,
+				mockConversionRepository,
+			)
+
+			conversion, err := serv.Pop(ctx)
+
+			if tc.err != nil {
+				assert.EqualError(t, err, tc.err.Error())
+			} else {
+				assert.NoError(t, err)
+				assert.Equal(t, conversion, tc.conversion)
+			}
+
+			mockConversionRepository.AssertExpectations(t)
+			mockTxManager.AssertExpectations(t)
+		})
+	}
+}
+
+func TestGetFromConversionQueue(t *testing.T) {
+	var (
+		conversion = &model.Conversion{
+			Fullpath: "/files/images/gen.jpg",
+			Path:     "/files/images",
+			Filestem: "gen",
+			Ext:      "jpg",
+		}
+	)
+
+	type testcase struct {
+		name                     string
+		err                      error
+		path                     string
+		conversion               *model.Conversion
+		mockConversionRepository func(tc *testcase) *repositoryMocks.MockConversionQueueRepository
+		mockTxManager            func(tc *testcase) *dbMocks.MockTxManager
+	}
+
+	cases := []testcase{
+		{
+			name:       "Item not found in conversion queue",
+			err:        db.ErrNotFound,
+			path:       "/path/to/file.ext",
+			conversion: conversion,
+			mockConversionRepository: func(tc *testcase) *repositoryMocks.MockConversionQueueRepository {
+				mockConversionRepository := repositoryMocks.NewMockConversionQueueRepository(t)
+				mockConversionRepository.On("FindByFullpath", mock.AnythingOfType("context.backgroundCtx"), tc.path).Return(nil, db.ErrNotFound)
+				return mockConversionRepository
+			},
+			mockTxManager: func(tc *testcase) *dbMocks.MockTxManager {
+				mockTxManager := dbMocks.NewMockTxManager(t)
+				return mockTxManager
+			},
+		},
+		{
+			name:       "Successful get from conversion queue",
+			path:       "/path/to/file.ext",
+			conversion: conversion,
+			mockConversionRepository: func(tc *testcase) *repositoryMocks.MockConversionQueueRepository {
+				mockConversionRepository := repositoryMocks.NewMockConversionQueueRepository(t)
+				mockConversionRepository.On("FindByFullpath", mock.AnythingOfType("context.backgroundCtx"), tc.path).Return(conversion, nil)
+				return mockConversionRepository
+			},
+			mockTxManager: func(tc *testcase) *dbMocks.MockTxManager {
+				mockTxManager := dbMocks.NewMockTxManager(t)
+				return mockTxManager
+			},
+		},
+	}
+
+	for _, tc := range cases {
+		t.Run(tc.name, func(t *testing.T) {
+			t.Parallel()
+
+			mockConversionRepository := tc.mockConversionRepository(&tc)
+			mockTxManager := tc.mockTxManager(&tc)
+
+			serv := conversionq.NewService(
+				config.MustLoad(configPath, defaultsPath),
+				mockTxManager,
+				mockConversionRepository,
+			)
+
+			conversion, err := serv.Get(ctx, tc.path)
+
+			if tc.err != nil {
+				assert.EqualError(t, err, tc.err.Error())
+			} else {
+				assert.NoError(t, err)
+				assert.Equal(t, conversion, tc.conversion)
+			}
+
+			mockConversionRepository.AssertExpectations(t)
+			mockTxManager.AssertExpectations(t)
+		})
+	}
+}
+
+func TestMarkAsDoneForConversionQueue(t *testing.T) {
+	type testcase struct {
+		name                     string
+		err                      error
+		path                     string
+		mockConversionRepository func(tc *testcase) *repositoryMocks.MockConversionQueueRepository
+		mockTxManager            func(tc *testcase) *dbMocks.MockTxManager
+	}
+
+	cases := []testcase{
+		{
+			name: "Item not found in conversion queue",
+			err:  db.ErrNotFound,
+			path: "/path/to/file.ext",
+			mockConversionRepository: func(tc *testcase) *repositoryMocks.MockConversionQueueRepository {
+				mockConversionRepository := repositoryMocks.NewMockConversionQueueRepository(t)
+				mockConversionRepository.On("MarkAsDone", mock.AnythingOfType("context.backgroundCtx"), tc.path).Return(db.ErrNotFound)
+				return mockConversionRepository
+			},
+			mockTxManager: func(tc *testcase) *dbMocks.MockTxManager {
+				mockTxManager := dbMocks.NewMockTxManager(t)
+				return mockTxManager
+			},
+		},
+		{
+			name: "Successful mark as done for conversion queue",
+			path: "/path/to/file.ext",
+			mockConversionRepository: func(tc *testcase) *repositoryMocks.MockConversionQueueRepository {
+				mockConversionRepository := repositoryMocks.NewMockConversionQueueRepository(t)
+				mockConversionRepository.On("MarkAsDone", mock.AnythingOfType("context.backgroundCtx"), tc.path).Return(nil)
+				return mockConversionRepository
+			},
+			mockTxManager: func(tc *testcase) *dbMocks.MockTxManager {
+				mockTxManager := dbMocks.NewMockTxManager(t)
+				return mockTxManager
+			},
+		},
+	}
+
+	for _, tc := range cases {
+		t.Run(tc.name, func(t *testing.T) {
+			t.Parallel()
+
+			mockConversionRepository := tc.mockConversionRepository(&tc)
+			mockTxManager := tc.mockTxManager(&tc)
+
+			serv := conversionq.NewService(
+				config.MustLoad(configPath, defaultsPath),
+				mockTxManager,
+				mockConversionRepository,
+			)
+
+			err := serv.MarkAsDone(ctx, tc.path)
+
+			if tc.err != nil {
+				assert.EqualError(t, err, tc.err.Error())
+			} else {
+				assert.NoError(t, err)
+			}
+
+			mockConversionRepository.AssertExpectations(t)
+			mockTxManager.AssertExpectations(t)
+		})
+	}
+}
+
+func TestMarkAsCanceledForConversionQueue(t *testing.T) {
+	type testcase struct {
+		name                     string
+		err                      error
+		path                     string
+		code                     uint32
+		mockConversionRepository func(tc *testcase) *repositoryMocks.MockConversionQueueRepository
+		mockTxManager            func(tc *testcase) *dbMocks.MockTxManager
+	}
+
+	cases := []testcase{
+		{
+			name: "Item not found in conversion queue",
+			err:  db.ErrNotFound,
+			path: "/path/to/file.ext",
+			code: service.ErrFileDoesNotExist,
+			mockConversionRepository: func(tc *testcase) *repositoryMocks.MockConversionQueueRepository {
+				mockConversionRepository := repositoryMocks.NewMockConversionQueueRepository(t)
+				mockConversionRepository.On("MarkAsCanceled", mock.AnythingOfType("context.backgroundCtx"), tc.path, tc.code).Return(db.ErrNotFound)
+				return mockConversionRepository
+			},
+			mockTxManager: func(tc *testcase) *dbMocks.MockTxManager {
+				mockTxManager := dbMocks.NewMockTxManager(t)
+				return mockTxManager
+			},
+		},
+		{
+			name: "Successful mark as canceled for conversion queue",
+			path: "/path/to/file.ext",
+			code: service.ErrFileDoesNotExist,
+			mockConversionRepository: func(tc *testcase) *repositoryMocks.MockConversionQueueRepository {
+				mockConversionRepository := repositoryMocks.NewMockConversionQueueRepository(t)
+				mockConversionRepository.On("MarkAsCanceled", mock.AnythingOfType("context.backgroundCtx"), tc.path, tc.code).Return(nil)
+				return mockConversionRepository
+			},
+			mockTxManager: func(tc *testcase) *dbMocks.MockTxManager {
+				mockTxManager := dbMocks.NewMockTxManager(t)
+				return mockTxManager
+			},
+		},
+	}
+
+	for _, tc := range cases {
+		t.Run(tc.name, func(t *testing.T) {
+			t.Parallel()
+
+			mockConversionRepository := tc.mockConversionRepository(&tc)
+			mockTxManager := tc.mockTxManager(&tc)
+
+			serv := conversionq.NewService(
+				config.MustLoad(configPath, defaultsPath),
+				mockTxManager,
+				mockConversionRepository,
+			)
+
+			err := serv.MarkAsCanceled(ctx, tc.path, tc.code)
+
+			if tc.err != nil {
+				assert.EqualError(t, err, tc.err.Error())
+			} else {
+				assert.NoError(t, err)
 			}
 
 			mockConversionRepository.AssertExpectations(t)
