0115af9d43fa780a9ac77d57e09933e44e587e7f
Ilya Chistyakov <ichistyakovv@gmail.com>
2024-12-23 15:43:48 +0500

    refactor: move transaction function to wrapper for testability

--- internal/service/deletionq/deletionq.go
+++ internal/service/deletionq/deletionq.go
@@ -37,11 +37,14 @@ func NewService(
 	}
 }
 
-func (s *serv) Add(ctx context.Context, info *model.DeletionInfo) (int64, error) {
-	// Skip checking if the file exists, as the source file might already be deleted when attempting removal.
-	var id int64
-
-	err := s.txManager.ReadCommitted(ctx, func(ctx context.Context) error {
+// Transactions are difficult to test when defined inside a method.
+// Use a wrapper function to enable testing of the transaction logic.
+// Initialize a new service with a nil txManager.
+// Create a mock txManager using the wrapper function, passing the newly created service.
+// Replace the txManager by invoking the SetTxManager method.
+// TODO: implement SetTxManager
+func AddTransaction(s *serv, id *int64, info *model.DeletionInfo) func(context.Context) error {
+	return func(ctx context.Context) error {
 		var errTx error
 		_, errTx = s.conversionRepository.FindByFullpath(ctx, info.Fullpath)
 		if errors.Is(errTx, db.ErrNotFound) {
@@ -59,10 +62,18 @@ func (s *serv) Add(ctx context.Context, info *model.DeletionInfo) (int64, error)
 		if !errors.Is(errTx, db.ErrNotFound) {
 			return errTx
 		}
-		id, errTx = s.deletionRepository.Create(ctx, info)
+		*id, errTx = s.deletionRepository.Create(ctx, info)
 
 		return errTx
-	})
+	}
+}
+
+func (s *serv) Add(ctx context.Context, info *model.DeletionInfo) (int64, error) {
+	// Skip checking if the file exists, as the source file might already be deleted when attempting removal.
+	var id int64
+
+	// Wrap transactin function to be able to test it
+	err := s.txManager.ReadCommitted(ctx, AddTransaction(s, &id, info))
 
 	if err != nil {
 		return -1, err
