046cb7fb9f8740e71f3478ae2670004da7b39483
Ilya Chistyakov <ichistyakovv@gmail.com>
2026-04-08 09:56:33 +0500

    chore: add TODO

--- cmd/converter/main.go
+++ cmd/converter/main.go
@@ -8,6 +8,7 @@ import (
 
 func main() {
 	ctx := context.Background()
+	// TODO: remove ctx from app and db client constructors and return error from http handler if context is canceled
 	a := app.NewApp(ctx)
 	a.Run(ctx)
 }
